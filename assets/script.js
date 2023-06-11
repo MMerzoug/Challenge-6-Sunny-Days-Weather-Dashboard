d73f9102852eea4fb5f358cc38a5e08c8addbf31
MMerzoug <monique.merzoug@icloud.com>
2023-06-10 22:31:57 -0700

    added innerHTML element to store city searches and created a function to clear them on refresh

--- assets/script.js
+++ assets/script.js
@@ -13,34 +13,55 @@ var wind = document.getElementById("wind");
 var humidity = document.getElementById("humidity");
 var nextWeather = document.getElementById("nextWeather");
 var nextWeatherContainer = document.getElementById("nextWeatherContainer");
-var nextDate = document.getElementById("nextDate");
-var nextTemp = document.getElementById("nextTemp");
-var nextWind = document.getElementById("nextWind");
-var nextHumidity = document.getElementById("nextHumidity");
+var citiesListContainer = document.getElementById("appendCitiesList");
+
+//Initialize cityStorage as an empty array
+var cityStorage = [];
 
 // event listener to trigger the next set of functions
 submitBtn.addEventListener("click", function () {
-    getCoords(userInput.value)
+    getCoords(userInput.value);
+    userInput.value = "";
 });
+
+function appendCitiesList() {
+    citiesListContainer.innerHTML = "";
+    for (let i = 0; i < cityStorage.length; i++) {
+        let cityName = cityStorage[i]
+        var cityButton = document.createElement("button");
+        cityButton.innerHTML = cityName
+        cityButton.onclick = function () {
+            getCoords(cityName);
+        }
+        citiesListContainer.appendChild(cityButton);
+    }
+}
 
 // function to get coordinates using the API call 
 function getCoords(city) {
     // geocoding API Call
     // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
     var url = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=fba06e5080ea71f1a51acc6fdb02dafb"
-    console.log(url);
-
+  
     fetch(url)
         .then(function (response) {
             return response.json();
 
         }).then(function (data) {
-            getCurrentWeather(data[0].lat, data[0].lon)
-
-            console.log(data[0].lat);
-        })
-
+            if (data[0]) {
+                cityStorage.unshift(city);
+                cityStorage = cityStorage.slice(0, 10);
+                appendCitiesList();
+                var lat = data[0].lat;
+                var lon = data[0].lon;
+                getCurrentWeather(lat, lon);
+                getNextWeather (lat,lon);
+            } else {
+                alert ("City not found. Please enter a valid city name.")
+            }
+        });
 }
+
 // lat and lon API Call
 // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 
@@ -56,20 +77,19 @@ function getCurrentWeather(lat, lon) {
         }).then(function (data) {
             // Call the function to display the weather data
             displayCurrentWeather(data);
-            console.log(data);
-        })
+        });
 }
 
 
 // function to display data in currentWeatherContainer
 var displayCurrentWeather = function (data) {
     city.textContent = data.name;
-    date.textContent = dayjs.unix(data.dt).format('MM/DD/YYYY');
+    date.innerHTML = dayjs.unix(data.dt).format("dddd") + "<br>" + "<br>" + dayjs.unix(data.dt).format("MM/DD/YYYY");
     temp.textContent = "Temperature: " + data.main.temp + " °F";
     wind.textContent = "Wind Speed: " + data.wind.speed + " MPH";
     humidity.textContent = "Humidity: " + data.main.humidity + " %";
 }
-console.log(displayCurrentWeather);
+
 
 //fba06e5080ea71f1a51acc6fdb02dafb
 // 5 day weather forecast API Key
@@ -79,24 +99,6 @@ console.log(displayCurrentWeather);
 
 //http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 //5 Day API call geocode 
-
-// function to get coordinates using the API call 
-function getCoords(city) {
-    var url = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=fba06e5080ea71f1a51acc6fdb02dafb"
-    console.log(url);
-
-    fetch(url)
-        .then(function (response) {
-            return response.json();
-
-        }).then(function (data) {
-            getCurrentWeather(data[0].lat, data[0].lon);
-            getNextWeather(data[0].lat, data[0].lon);
-
-            console.log(data[0].lat);
-        })
-
-}
 
 
 // function to get 5 day forecast 
@@ -112,15 +114,16 @@ function getNextWeather(lat, lon) {
             // Call the function to display the weather data
             displayNextWeather(data);
             console.log(data);
-        })
+        });
 }
 
 // function to display data in NextWeatherContainer
 var displayNextWeather = function (data) {
     nextWeatherContainer.innerHTML = ""; // Clear the forecast container
-    for (let i = 0; i < data.list.length; i++) {
+    for (let i = 2; i < data.list.length; i += 8) {
         var forecastPeriod = document.createElement("section");
         forecastPeriod.innerHTML = `
+        <h2>${dayjs.unix(data.list[i].dt).format('dddd')}</h2>
         <h2>${dayjs.unix(data.list[i].dt).format('MM/DD/YYYY')}</h2>
         <p>Temperature: ${data.list[i].main.temp} °F</p>
         <p>Wind Speed: ${data.list[i].wind.speed} MPH</p>
@@ -129,5 +132,5 @@ var displayNextWeather = function (data) {
         nextWeatherContainer.appendChild(forecastPeriod);
     }
 }
-console.log(displayNextWeather);
+
 // use bootstrap to style
